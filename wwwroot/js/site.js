c62d4dc5cc2a31c81d6528ca030726c1aea64784
LuukyPuuky <537956@student.fontys.nl>
2024-06-14 14:00:16 +0200

    Wip navbar

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,88 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+document.addEventListener("DOMContentLoaded", function () {
+  var calendarEl = document.getElementById("calendar");
+
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    timeZone: "UTC",
+    initialView: "dayGridWeek",
+    headerToolbar: {
+      left: "prev,next",
+      center: "title",
+      right: "dayGridWeek,dayGridDay",
+    },
+    editable: true,
+    events: "https://fullcalendar.io/api/demo-feeds/events.json",
+  });
+
+  calendar.render();
+});
+
+// Clock
+function startTime() {
+  const today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById("clock").innerHTML = h + ":" + m + ":" + s;
+  setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
+// Clock
+
+// Adding chart.js to the document Javascript:
+const newbar = document.getElementById("bar");
+
+new Chart(newbar, {
+  type: "bar",
+  data: {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
+
+const newpie = document.getElementById("pie");
+
+new Chart(newpie, {
+  type: "pie",
+  data: {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
